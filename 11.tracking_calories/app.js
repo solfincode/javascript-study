496d9446268332624f9a4c132ff2d6437a8fd562
Q <coderinch@gmail.com>
2020-09-07 19:34:00 +0900

    add edit state

--- 11.tracking_calories/app.js
+++ 11.tracking_calories/app.js
@@ -38,6 +38,21 @@ const ItemCtrl = (function () {
 			data.items.push(newItem);
 			return newItem;
 		},
+		getItemById: function (id) {
+			let found = null;
+			data.items.forEach(function (item) {
+				if (item.id === id) {
+					found = item;
+				}
+			});
+			return found;
+		},
+		setCurrentItem: function (item) {
+			data.currentItem = item;
+		},
+		getCurrentItem: function () {
+			return data.currentItem;
+		},
 		getTotalCalories: function () {
 			let total = 0;
 			data.items.forEach(function (item) {
@@ -56,6 +71,9 @@ const UICtrl = (function () {
 	const UISelectors = {
 		itemList: "#item-list",
 		addBtn: "#addBtn",
+		updateBtn: "#update-btn",
+		deleteBtn: "#delete-btn",
+		backBtn: "#back-btn",
 		itemNameInput: "#item-name",
 		itemCaloriesInput: "#item-calories",
 		totalCalories: ".total-calories",
@@ -84,7 +102,7 @@ const UICtrl = (function () {
 			li.className = "collection-item";
 			li.id = `item-${item.id}`;
 			li.innerHTML = `<strong>${item.name}:</strong><em>${item.calories} calories</em>
-            <a href="#" class="secondary-content">delete</a>`;
+            <a href="#" class="secondary-content">edit</a>`;
 			document
 				.querySelector(UISelectors.itemList)
 				.insertAdjacentElement("beforeend", li);
@@ -93,6 +111,15 @@ const UICtrl = (function () {
 			document.querySelector(UISelectors.itemNameInput).value = "";
 			document.querySelector(UISelectors.itemCaloriesInput).value = "";
 		},
+		addItemToForm: function () {
+			document.querySelector(
+				UISelectors.itemNameInput
+			).value = ItemCtrl.getCurrentItem().name;
+			document.querySelector(
+				UISelectors.itemCaloriesInput
+			).value = ItemCtrl.getCurrentItem().calories;
+			UICtrl.showEditState();
+		},
 		hideList: function () {
 			document.querySelector(UISelectors.itemList).style.display = "none";
 		},
@@ -100,6 +127,19 @@ const UICtrl = (function () {
 			document.querySelector(
 				UISelectors.totalCalories
 			).textContent = totalCalories;
+		},
+		clearEditState: function () {
+			UICtrl.clearInput();
+			document.querySelector(UISelectors.updateBtn).style.display = "none";
+			document.querySelector(UISelectors.deleteBtn).style.display = "none";
+			document.querySelector(UISelectors.backBtn).style.display = "none";
+			document.querySelector(UISelectors.addBtn).style.display = "inline";
+		},
+		showEditState: function () {
+			document.querySelector(UISelectors.updateBtn).style.display = "inline";
+			document.querySelector(UISelectors.deleteBtn).style.display = "inline";
+			document.querySelector(UISelectors.backBtn).style.display = "inline";
+			document.querySelector(UISelectors.addBtn).style.display = "none";
 		},
 		getSelectors: function () {
 			return UISelectors;
@@ -112,10 +152,12 @@ const App = (function (ItemCtrl, UICtrl) {
 	const loadEventListeners = function () {
 		const UISelectors = UICtrl.getSelectors();
 		//add item event
-
 		document
 			.querySelector(UISelectors.addBtn)
 			.addEventListener("click", itemAddSubmit);
+		document
+			.querySelector(UISelectors.itemList)
+			.addEventListener("click", itemEditClick);
 	};
 
 	const itemAddSubmit = function (e) {
@@ -133,10 +175,26 @@ const App = (function (ItemCtrl, UICtrl) {
 		}
 		e.preventDefault();
 	};
+	const itemEditClick = function (e) {
+		if (e.target.classList.contains("secondary-content")) {
+			const listId = e.target.parentNode.id;
+			//break into array
+			const listIdArray = listId.split("-");
+			//get actual id
+			const id = parseInt(listIdArray[1]);
+			//get item
+			const itemToEdit = ItemCtrl.getItemById(id);
+			//set current item
+			ItemCtrl.setCurrentItem(itemToEdit);
+			UICtrl.addItemToForm();
+		}
+		e.preventDefault();
+	};
 	//public methods
 	return {
 		init: function () {
-			console.log("initializing app...");
+			// clear edit state
+			UICtrl.clearEditState();
 			//fetch item from data structure
 			const items = ItemCtrl.getItems();
 			//check if any items
